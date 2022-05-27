41d62d1586e9701981a25622ab0630ddc61645bb
tomasz8w <td.wieclaw@gmail.com>
2022-05-27 16:00:04 +0200

    Login page mockup init

--- src/app/login/Login.tsx
+++ src/app/login/Login.tsx
@@ -1,28 +1,70 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
 
-import { AppRoute } from 'routing/AppRoute.enum';
+import { Box, Button, ButtonBase, TextField, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { AppRoute } from "routing/AppRoute.enum";
 
 export const Login = () => {
   return (
-    <>
-      <Link to={AppRoute.Home}>Products page</Link>
-      <h2>Login</h2>
-      <form>
-        <div>
-          <label>
-            username:
-            <input name="username" />
-          </label>
-        </div>
-        <div>
-          <label>
-            password:
-            <input name="password" type="password" />
-          </label>
-        </div>
-        <button type="submit">submit</button>
-      </form>
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        backgroundColor: "background.paper",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+          maxWidth: "500px",
+          px: 3,
+          py: 5,
+        }}
+      >
+        <ButtonBase
+          component={Link}
+          to={AppRoute.Home}
+          sx={{ justifyContent: "left" }}
+        >
+          <Typography variant="h2">join.tsh.io</Typography>
+        </ButtonBase>
+        <Box
+          component="form"
+          sx={{ display: "flex", flexDirection: "column", gap: 2, mt: "12vh" }}
+        >
+          <Typography variant="h1">Login</Typography>
+          <Box>
+            <Typography sx={{ py: 1 }} variant="body2">
+              Username
+            </Typography>
+            <TextField fullWidth placeholder="Enter username" id="username" />
+          </Box>
+          <Box>
+            <Typography sx={{ py: 1 }} variant="body2">
+              Password
+            </Typography>
+            <TextField
+              fullWidth
+              placeholder="Enter password"
+              id="password"
+              type="password"
+            />
+          </Box>
+          <Button
+            sx={{ mt: 6, mb: 1 }}
+            variant="contained"
+            size="large"
+            fullWidth
+          >
+            Log in
+          </Button>
+          <Typography variant="caption">Forgot password?</Typography>
+        </Box>
+      </Box>
+    </Box>
   );
 };
